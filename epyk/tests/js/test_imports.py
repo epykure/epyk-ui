e102814c4d8f9bd34ed62d59c40eeb1451da3871
pyotr <smith.pyotr@gmail.com>
2019-09-15 19:46:09 +0100

    Tests js

---
+++ epyk/tests/js/test_imports.py
@@ -0,0 +1,28 @@
+""" """
+
+import collections
+
+from epyk.core.js import Imports
+from epyk.core.Ares import Report
+
+#Imports.installed_packages()
+
+
+report = Report()
+imports = Imports.ImportManager(report=report)
+
+modules = collections.OrderedDict()
+print(imports.getModules(modules, 'c3'))
+
+i = imports.cleanImports(['c3'], Imports.JS_IMPORTS)
+print(i)
+
+i = imports.cssResolve(['c3'])
+print(i)
+
+f = imports.getFiles(['c3'], ['c3'])
+print(f['css'][0]['file']['script'])
+#
+
+p = imports.getPackage('jqueryui', static_path=r"..\outs")
+p = imports.getFullPackage('font-awesome', static_path=r"..\outs")
--- epyk/tests/js/test_js_primitives.py
+++ epyk/tests/js/test_js_primitives.py
@@ -1,60 +1,58 @@
-
 from epyk.core.js import Js
 from epyk.core.js import JsUtils
-from epyk.core.js.primitives import JsNumber
+
+from epyk.core.js.primitives import JsDate
+from epyk.core.js.primitives import JsString
 
 
+# ------------------------------------------------------------------------------------------------------------------
+#
 jsObj = Js.JsBase()
-
-print(jsObj.objects.number.new(349.673, "MyNumber").toStr())
-
-
-
-jsObj = Js.JsBase()
-f = JsUtils.JsFile(os.path.basename(__file__).split(".")[0])
+f = JsUtils.JsFile("Array", path=r"../outs")
 f.writeJs([
-jsObj.objects.array.new([2, 5, 12, -3], "MyArray"),
-jsObj.objects.array.new([3, -9, 2, -6], "MyArray2"),
-jsObj.objects.array.new([], "MyArray3"),
-jsObj.console.log(jsObj.objects.array.get("MyArray3").concat(jsObj.objects.array.get("MyArray"), jsObj.objects.array.get("MyArray2"))),
-jsObj.console.log(jsObj.objects.array.get("MyArray").findIndex([
-  jsObj.if_(jsObj.data.loop.val <= 0, [
-    jsObj.return_(jsObj.objects.true)]).else_(jsObj.return_(jsObj.objects.false)),
-])),
+  jsObj.objects.array.new([2, 5, 12, -3], "MyArray"),
+  jsObj.objects.array.new([3, -9, 2, -6], "MyArray2"),
+  jsObj.objects.array.new([], "MyArray3"),
+  jsObj.console.log(jsObj.objects.array.get("MyArray3").concat(jsObj.objects.array.get("MyArray"), jsObj.objects.array.get("MyArray2"))),
+  jsObj.console.log(jsObj.objects.array.get("MyArray").findIndex([
+    jsObj.if_(jsObj.data.loop.val <= 0, [
+      jsObj.return_(jsObj.objects.true)]).else_(jsObj.return_(jsObj.objects.false)),
+  ])),
 
-jsObj.console.log(jsObj.objects.array.get("MyArray").map([
-  jsObj.data.loop.val * jsObj.math.max(jsObj.data.loop.arr.toArgs()),
-  jsObj.return_(jsObj.data.loop.val)
-])),
-jsObj.console.log("########"),
-# jsObj.console.log(jsObj.objects.array.get("MyArray")),
-# jsObj.console.log(jsObj.objects.array.get("MyArray").reverse()),
-# jsObj.console.log(jsObj.objects.array.get("MyArray").push(55, -17)),
-# jsObj.console.log(jsObj.objects.array.get("MyArray")),
-# jsObj.console.log(jsObj.objects.array.get("MyArray").pop()),
-# jsObj.console.log(jsObj.objects.array.get("MyArray").join("_")),
-# jsObj.console.log(jsObj.objects.array.get("MyArray").slice(3, 5)),
-# jsObj.console.log(jsObj.objects.array.get("MyArray").sort()),
-jsObj.console.log(jsObj.objects.array.new([2, 5, 12, -3], "MyArray").shift()),
-jsObj.console.log(jsObj.objects.array.get("MyArray").delete(2)),
-jsObj.console.log(jsObj.objects.array.get("MyArray").unshift(22)),
-#jsObj.console.log(jsObj.objects.array.get("MyArray").fill("Olivier", 0, 5)),
-jsObj.console.log(jsObj.objects.array.get("MyArray")),
-jsObj.objects.array.get("MyArray").copyWithin().setVar("youpi"),
-#jsObj.console.log(jsObj.objects.array.get("youpi").append(jsObj, 34).append(jsObj, -47)),
-#jsObj.while_()
-# jsObj.console.log(jsObj.objects.array.get("MyArray").reduce([
-#     jsObj.data.reduce.val + jsObj.data.reduce.rVal,
-#     jsObj.return_(jsObj.data.reduce.val)
-#   ]).add(2)
-#)
+  jsObj.console.log(jsObj.objects.array.get("MyArray").map([
+    jsObj.data.loop.val * jsObj.math.max(jsObj.data.loop.arr.toArgs()),
+    jsObj.return_(jsObj.data.loop.val)
+  ])),
+  jsObj.console.log("########"),
+  # jsObj.console.log(jsObj.objects.array.get("MyArray")),
+  # jsObj.console.log(jsObj.objects.array.get("MyArray").reverse()),
+  # jsObj.console.log(jsObj.objects.array.get("MyArray").push(55, -17)),
+  # jsObj.console.log(jsObj.objects.array.get("MyArray")),
+  # jsObj.console.log(jsObj.objects.array.get("MyArray").pop()),
+  # jsObj.console.log(jsObj.objects.array.get("MyArray").join("_")),
+  # jsObj.console.log(jsObj.objects.array.get("MyArray").slice(3, 5)),
+  # jsObj.console.log(jsObj.objects.array.get("MyArray").sort()),
+  jsObj.console.log(jsObj.objects.array.new([2, 5, 12, -3], "MyArray").shift()),
+  jsObj.console.log(jsObj.objects.array.get("MyArray").delete(2)),
+  jsObj.console.log(jsObj.objects.array.get("MyArray").unshift(22)),
+  #jsObj.console.log(jsObj.objects.array.get("MyArray").fill("Olivier", 0, 5)),
+  jsObj.console.log(jsObj.objects.array.get("MyArray")),
+  jsObj.objects.array.get("MyArray").copyWithin().setVar("youpi"),
+  #jsObj.console.log(jsObj.objects.array.get("youpi").append(jsObj, 34).append(jsObj, -47)),
+  #jsObj.while_()
+  # jsObj.console.log(jsObj.objects.array.get("MyArray").reduce([
+  #     jsObj.data.reduce.val + jsObj.data.reduce.rVal,
+  #     jsObj.return_(jsObj.data.reduce.val)
+  #   ]).add(2)
+  #)
 ])
 f.close()
 
 
-# Boolean
+# ------------------------------------------------------------------------------------------------------------------
+#
 jsObj = Js.JsBase()
-f = JsUtils.JsFile(os.path.basename(__file__).split(".")[0])
+f = JsUtils.JsFile("Boolean", path=r"../outs")
 f.writeJs([
   jsObj.objects.boolean.new(False, "testBool"),
   jsObj.console.log(jsObj.objects.boolean.get("testBool")),
@@ -64,16 +62,16 @@ f.writeJs([
 f.close()
 
 
-# Dates
+# ------------------------------------------------------------------------------------------------------------------
+#
+f = JsUtils.JsFile("Date", path=r"../outs")
 
-from epyk.core.js import Js
-
-f = JsUtils.JsFile("jsDate")
-
-dt = JsDate.new("2019-05-03")
+dt = JsDate.JsDate.new("2019-05-03")
 f.writeJs([dt,
   Js.JsConsole().log(dt.getDay()),
-  Js.JsConsole().log(dt.getFullYear())])
+  Js.JsConsole().log(dt.getFullYear())
+])
+
 
 jsObj = Js.JsBase()
 domObj = jsObj.createElement("a").text("Toupi 4").attr({"href": 'google', "title": 'google2'}).css({"color": "red"})
@@ -95,15 +93,10 @@ f.writeJs(jsObj.body.appendChild(domObj))
 f.close(jsObj)
 
 
-# Number
-
-import os
-
-from epyk.core.js import Js
-from epyk.core.js import JsUtils
-
+# ------------------------------------------------------------------------------------------------------------------
+#
 jsObj = Js.JsBase()
-f = JsUtils.JsFile(os.path.basename(__file__).split(".")[0])
+f = JsUtils.JsFile("Numbers2", path=r"../outs")
 f.writeJs([
   jsObj.objects.number.new(349.673, "MyNumber"),
   jsObj.console.log(jsObj.objects.number.get("MyNumber").toPrecision(2)),
@@ -115,13 +108,10 @@ f.writeJs([
 f.close()
 
 
-# Object
-
-import os
-from epyk.core.js import Js
-
+# ------------------------------------------------------------------------------------------------------------------
+#
 jsObj = Js.JsBase()
-f = JsUtils.JsFile(os.path.basename(__file__).split(".")[0])
+f = JsUtils.JsFile("Object", path=r"../outs")
 f.writeJs([
   jsObj.objects.new({}, varName="MyObject"),
   jsObj.objects.number.new(13, varName="MyNumber"),
@@ -139,16 +129,12 @@ f.writeJs([
 f.close()
 
 
-
-# String
-
-import os
-from epyk.core.js import Js
-
+# ------------------------------------------------------------------------------------------------------------------
+#
 jsObj = Js.JsBase()
-f = JsUtils.JsFile(os.path.basename(__file__).split(".")[0])
+f = JsUtils.JsFile("String", path=r"../outs")
 
-string = JsString.new("te st", "testStr")
+string = JsString.JsString.new("te st", "testStr")
 f.writeJs([
 #print(string.length.toStr())
   string,
@@ -157,7 +143,7 @@ f.writeJs([
   string.endsWith("st"),
   string.charCodeAt(2),
   string.freeze(),
-  JsString.new("2019-08-03", "testDate").toDate().setVar("MyDate"),
+  JsString.JsString.new("2019-08-03", "testDate").toDate().setVar("MyDate"),
   jsObj.console.log(jsObj.objects.date.get("MyDate").addDays(jsObj, 6).getStrDate())
 ])
 f.close(jsObj)
