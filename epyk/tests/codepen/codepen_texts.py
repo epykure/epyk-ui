325a67f43934be274d9c145675f981c32e2de40e
pyotr <smith.pyotr@gmail.com>
2019-09-21 21:31:04 +0100

    Update codepen_texts.py

--- epyk/tests/codepen/codepen_texts.py
+++ epyk/tests/codepen/codepen_texts.py
@@ -4,9 +4,13 @@ from epyk.tests import test_statics
 
 rptObj = Report()
 
-rptObj.ui.texts.text("Test text").css({"color": 'red'})
+text = rptObj.ui.texts.text("Test text").css({"color": 'red'})
 rptObj.ui.texts.number(188448228, label="test")
 rptObj.ui.texts.title("title").css({"color": 'green'})
 rptObj.ui.texts.highlights("This is a news", title="Important news")
+
+
+button = rptObj.ui.button("click me")
+button.click([rptObj.js.window.alert("ok"), text.jsGenerate("Youpi", isPyData=True)])
 
 rptObj.outs.browser.codepen(path=test_statics.OUTPUT_PATHS)
