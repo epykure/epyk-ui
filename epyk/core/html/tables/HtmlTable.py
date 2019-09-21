1a9b4bbfcba0aecbc88db5d975b30e9768f62610
pyotr <smith.pyotr@gmail.com>
2019-09-21 15:48:07 +0100

    Add HTML tables

---
+++ epyk/core/html/tables/HtmlTable.py
@@ -0,0 +1,197 @@
+"""
+Base module for the tables
+"""
+
+import json
+
+from epyk.core.html import Html
+
+
+class Row(Html.Html):
+
+  @staticmethod
+  def to_html(row, css=None, css_cols=None, header=False):
+    _row = []
+    css_cols = css_cols or {}
+    for i, _cell in enumerate(row):
+      css_cell = dict(css)
+      if i in css_cols:
+        css_cell.update(css_cols[i])
+      _row.append(Cell.to_html(_cell, css=css_cell, header=header))
+    if css is not None:
+      style = ["%s:%s" % (k, v) for k, v in css.items()]
+      return "<tr style='%s'>%s</tr>" % (";".join(style), "".join(_row))
+
+    return "<tr>%s</tr>" % "".join(_row)
+
+  def __str__(self):
+    return "<tr %s></tr>" % (self.strAttr(pyClassNames=self.pyStyle))
+
+
+class Cell(Html.Html):
+
+  @staticmethod
+  def to_html(cell, css=None, header=False):
+    style = ["%s:%s" % (k, v) for k, v in css.items()] if css is not None else []
+    if header:
+      return "<th style='%s'>%s</th>" % (";".join(style), cell)
+
+    return "<td style='%s'>%s</td>" % (";".join(style), cell)
+
+  def __str__(self):
+    return "<td %s></td>" % (self.strAttr(pyClassNames=self.pyStyle))
+
+
+class Bespoke(Html.Html):
+  name, category, callFnc = 'Basic', 'Tables', 'basic'
+  __pyStyle = ['CssDataTable', 'CssDataTableHeader', 'CssDataTableEven', 'CssDataTableOdd']
+
+  def __init__(self, report, recordSet, cols, rows, width, height, htmlCode, options, profile):
+    data = []
+    self.header = rows + cols
+    for rec in recordSet:
+      data.append([rec[c] for c in self.header])
+    super(Bespoke, self).__init__(report, data, code=htmlCode, width=width[0], widthUnit=width[1], height=height[0],
+                                  heightUnit=height[1], profile=profile)
+    self.css({"text-align": 'center', 'border-collapse': 'collapse'})
+    self._style = {"rows": {"padding": '5px 0'}, "header": {"padding": '5px 0'}}
+
+  def row_style(self, css, row_id=None):
+    """
+    Change the style of a particular row in the table
+
+    Example
+    simple_table.row_style({"color": 'red'}, row_id=3)
+
+    :param css: A dictionary with the CSS Style
+    :param row_id: Optional. The row id
+
+    :return: The python table object
+    """
+    if row_id is None:
+      self._style["rows"] = css
+    else:
+      self._style.setdefault("row", {})[row_id] = css
+    return self
+
+  def column_style(self, css, column_name=None):
+    """
+    Change the style of a particular column in the table
+
+    Example
+    simple_table.column_style({"color": 'pink'}, column_name="A")
+
+    :param css: A dictionary with the CSS styles
+    :param column_name: Optional. The column name
+
+    :return: The python table object
+    """
+    if column_name is None:
+      self._style["cols"] = css
+    else:
+      col_index = self.header.index(column_name)
+      self._style.setdefault("cols", {})[col_index] = css
+    return self
+
+  def column_format(self, fnc, column_name):
+    """
+
+    :param fnc:
+    :param column_name:
+    :return:
+    """
+    return self
+
+  def row_add(self, data, missing=""):
+    """
+    Add a row to the table
+
+    Example
+    simple_table.row_add({"column": "value"})
+
+    :param data: The row to be added to the table
+    :param missing: The data to put when a cell is missing
+
+    :return: The python table
+    """
+    if isinstance(data, dict):
+      self.vals.append([data.get(h, missing) for h in self.header])
+    else:
+      self.vals.append(data)
+    return self
+
+  def __str__(self):
+    _data = ["<thead>%s</thead><tbody>" % Row.to_html(self.header, css=self._style["header"], header=True)]
+    for i, _row in enumerate(self.vals):
+      css_row = dict(self._style["rows"])
+      if i in self._style.get('row', []):
+        css_row.update(self._style['row'][i])
+      _data.append(Row.to_html(_row, css=css_row, css_cols=self._style.get('cols', {})))
+    _data.append("</tbody>")
+    return "<table %s>%s</table>" % (self.strAttr(pyClassNames=self.pyStyle), "".join(_data))
+
+
+class Excel(Html.Html):
+  name, category, callFnc = 'Excel', 'Tables', 'excel'
+  __pyStyle = ['CssTableExcel', 'CssTableExcelHeaderCell', 'CssTableExcelTd']
+
+  def __init__(self, report, recordSet, cols, rows, title, width, height, cellwidth, delimiter, htmlCode):
+    self.recordSet, self.delimiter = recordSet, delimiter
+    super(Excel, self).__init__(report, [], code=htmlCode, width=width[0], widthUnit=width[1], height=height[0], heightUnit=height[1])
+    self._jsStyles = {'header': rows + cols, 'cellwidth': cellwidth}
+    self.css({'display': 'inline-block', 'overflow': 'auto', 'padding': 0, 'vertical-align': 'top'})
+    self.add_title(title)
+
+  @property
+  def val(self):
+    return "JSON.stringify(tableData(%s))" % self.jqId
+
+  @property
+  def records(self):
+    return "listToRec(tableData(%s), %s)" % (self.jqId, json.dumps(self._jsStyles['header']))
+
+  @property
+  def jqId(self):
+    return "$('#%s table')" % self.htmlId
+
+  def onDocumentLoadFnc(self):
+    self.addGlobalFnc("%s(htmlObj, data, jsStyles)" % self.__class__.__name__, ''' htmlObj.empty();
+      var tr = $('<tr></tr>');
+      jsStyles.header.forEach(function(rec){tr.append("<th>"+ rec +"</th>")});
+      htmlObj.append(tr); var tr = $('<tr></tr>'); var tbody = $('<tbody></tbody>');
+      jsStyles.header.forEach(function(rec){tr.append("<td><input type='text' style='"+ jsStyles.cellwidth +"'/></td>")});
+      tbody.append(tr);htmlObj.append(tbody)''')
+
+  def __str__(self):
+    self._report.jsOnLoadFnc.add('''
+      function tableData(tableObj){
+        res = [];
+        tableObj.find('tbody').find('tr').each(function(key, val){
+          var row = [];
+          $(this).find('td').each(function(key, cell) {row.push($(cell).find('input').val())});
+          res.push(row)}); return res};
+
+      function listToRec(data, header){
+          var res = [];
+          data.forEach(function(row){
+            rec = {};
+            header.forEach(function(h, i){rec[h] = row[i]}); res.push(rec);
+          }); return res}''')
+
+    self.paste('''
+      var tbody = $(this).find('tbody'); tbody.empty();
+      var tableId = $(this).parent().attr('id');
+      var lineDelimiter = $('#'+ tableId +'_delimiter').val();
+      if (lineDelimiter == 'TAB'){lineDelimiter = '\\t'};
+      data.split("\\n").forEach(function(line){
+        if (line !== ''){
+          var tr = $('<tr></tr>');
+          line.split(lineDelimiter).forEach(function(rec){tr.append("<td><input type='text'  value='"+ rec +"'/></td>")
+        }); tbody.append(tr)}}) ''')
+    if self.delimiter is None:
+      delimiter = '<input id="%s_delimiter" type="text" value="%s" placeholder="Line delimiter"/>' % (self.htmlId, self.delimiter)
+    else:
+      delimiter = '<input id="%s_delimiter" type="text" value="%s" style="display:none" placeholder="Line delimiter"/>' % (
+          self.htmlId, self.delimiter)
+    return '<div %(strAttr)s>%(delimiter)s<table style="width:100%%"></table></div>' % {
+      'strAttr': self.strAttr(pyClassNames=self.pyStyle), 'delimiter': delimiter}
---
+++ epyk/core/html/tables/HtmlTablePivot.py
@@ -0,0 +1,166 @@
+"""
+Module in charge of the PivotTable library
+
+https://pivottable.js.org/examples/
+"""
+
+import json
+
+from epyk.core.html import Html
+from epyk.core.js.objects import JsPivotFncs
+
+extensions = {
+  'sub-total': {'jsImports': ['pivot-sub-total']},
+  'c3': {'jsImports': ['pivot-c3']},
+}
+
+
+class PivotTable(Html.Html):
+  __reqJs, __reqCss = ["pivot"], ["pivot"]
+  name, category, callFnc = 'Pivot Table', 'Tables', 'pivot'
+  __pyStyle = ['CssPivotHead', 'CssPivotCells', 'CssPivotFilterBox', 'CssPivotAxis', 'CssPivotFilterVals',
+               'CssPivotFilterBoxPopUp']
+
+  def __init__(self, report, recordSet, rows, cols, valCol, title, tableOptions, width, height, aggOptions, rendererName,
+               htmlCode, dataSrc, helper, profile):
+    super(PivotTable, self).__init__(report, [], width=width[0], widthUnit=width[1], height=height[0], heightUnit=height[1],
+                                     code=htmlCode, dataSrc=dataSrc, profile=profile)
+    # Add the extra HTML components
+    self.add_title(title)
+    self.add_helper(helper)
+    # to add all the columns in the table if nothing defined
+    self.aggOptions, self.dsc, self.tableOptions, self.addinOptions = aggOptions, '', dict(tableOptions), []
+    self.__pivot = {'cols': [] if cols is None else cols, 'rows': [] if rows is None else rows, 'vals': [] if valCol is None else valCol,
+                    'aggregatorName': aggOptions['name'], 'rendererName': rendererName}
+    self.__aggFncs = dict(JsPivotFncs.getAggFnc())
+    self.data = recordSet
+    self.data.attach(self)
+    self.css({"overflow": 'auto', 'margin': '0 auto',})
+    self.addGlobalFnc("numberWithCommas(x)", 'return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")')
+    if not self.tableOptions.get("editable", True):
+      self.addinOptions.append("window['options_%s'].showUI = false" % self.htmlId)
+      del self.tableOptions["editable"]
+
+    if self.tableOptions.get('sub-total'):
+      self._report.jsImports.add('pivot-sub-total')
+      self.addinOptions.append("window['options_%s'].dataClass = $.pivotUtilities.SubtotalPivotData" % self.htmlId)
+      self.addinOptions.append("window['options_%s'].renderers = $.pivotUtilities.subtotal_renderers" % self.htmlId)
+      self.addinOptions.append("window['options_%s'].rendererName = 'Table With Subtotal'" % self.htmlId)
+      del tableOptions['sub-total']
+
+    # https://pivottable.js.org/examples/onrefresh.html
+    for k, v in self.tableOptions.items():
+      if k == 'sorters':
+        row = []
+        for i, j in v.items():
+          if isinstance(j, list):
+            row.append("'%s': $.pivotUtilities.sortAs(%s)" % (i, j))
+          else:
+            row.append("'%s': %s" % (i, j))
+        self.addinOptions.append("window['options_%(htmlId)s'].%(opt)s = {%(val)s}" % {'htmlId': self.htmlId, 'opt': k, 'val': ",".join(row)})
+      else:
+        self.addinOptions.append("window['options_%(htmlId)s'].%(opt)s = %(val)s" % {'htmlId': self.htmlId, 'opt': k, 'val': json.dumps(v)})
+
+  @property
+  def jqId(self):
+    return "$('#%s div')" % self.htmlId
+
+  def addAggregator(self, aggCls):
+    """
+    Add on the fly new aggregation logic to the pivot table. This will allow the creation of bespoke aggregation functions.
+    Those functions will be structure in the way they can be added to the core framework easily.
+    Aggregators should follow some rules and they should defined some mandatory parameters (name, keyAgg, push, value, format).
+
+    Example
+      class JsPivotSumAgg(object):
+        name = "New Sum Agg"
+        keyAgg, key2Agg = 0, None
+        push = 'this.keyAgg += parseFloat(record[attributeArray[0]]) * 2'
+        value = 'return this.keyAgg'
+        format = 'return numberWithCommas(x.toFixed(%(digits)s))'
+
+    Documentation
+    https://github.com/nicolaskruchten/pivottable/wiki/Aggregators
+    """
+    if not ':dsc:' in aggCls.__doc__:
+      raise Exception("The new Aggregator class %s must have a doc string with a :dsc: field !" % aggCls.name)
+
+    if aggCls.name in self.__aggFncs:
+      raise Exception("Duplicated Name - Aggregator %s cannot be replaced !!!" % aggCls.name)
+
+    self.__aggFncs[aggCls.name] = type(aggCls.__name__, (aggCls, JsPivotFncs.JsPivotAggFnc), {})()
+    return self
+
+  def jsGenerate(self, jsData='data', jsDataKey=None, isPyData=False, jsId=None):
+    return """%(jqId)s.pivotUI(%(jsData)s, window['options_%(htmlId)s'])""" % {'jqId': self.jqId, 'jsData': self.data.setId(jsData).getJs(), 'htmlId': self.htmlId}
+
+  def onDocumentReady(self):
+    jsAggFncs = "{%s}" % ", ".join(["'%s': function(attributeArray) {return function(data, rowKey, colKey) {return %s}}" % (name, aggFncs.toJs(self.aggOptions)) for name, aggFncs in self.__aggFncs.items()])
+    profile = self.profile if self.profile is not None else getattr(self._report, 'PROFILE', False)
+    preFnc, endFnc = "", ""
+    if profile:
+      preFnc, endFnc = "var t0 = performance.now()", "console.log('|%s|%s|'+ (performance.now()-t0))" % (self.__class__.__name__, self.htmlId)
+    self._report.jsOnLoadFnc.add(""" %(preFnc)s;
+      var tpl = $.pivotUtilities.aggregatorTemplates; window['options_%(htmlId)s'] = %(options)s; %(addinOptions)s;
+      window['options_%(htmlId)s'].aggregators = %(agg)s;
+      window['options_%(htmlId)s'].onRefresh = function (config) {
+          %(jqId)s.find('.pvtVal').each(function( index, items ) {
+            if (parseFloat(items.innerText.replace(',', '.')) < 0){ $(items).css('color', 'red')}});
+          %(jqId)s.find('.pvtTotal').each(function( index, items ) {
+            if (parseFloat(items.innerText.replace(',', '.')) < 0){ $(items).css('color', 'red')}});
+          %(jqId)s.find('.pvtGrandTotal').each(function( index, items ) {
+            if (parseFloat(items.innerText.replace(',', '.')) < 0){ $(items).css('color', 'red')}});
+      } """ % {'jqId': self.jqId, 'options': json.dumps(self.__pivot), 'agg': jsAggFncs, 'htmlId': self.htmlId,
+               'addinOptions': ";".join(self.addinOptions), "preFnc": preFnc})
+    self._report.jsOnLoadFnc.add("%s;%s" % (self.jsGenerate(None), endFnc))
+
+  def onDocumentLoadFnc(self): return True
+
+  def setTableCss(self, cssClss):
+    """
+    Add a style to the datatable. This can be used to change some specific part of the table (for example the header)
+
+    Example
+      class CssTableHeader(object):
+        __style = [{'attr': 'background', 'value': 'grey'}]
+        childrenTag = 'th'
+
+      tb.setTableCss(CssTableHeader)
+
+    :return: The Python Datatable object
+    """
+    import inspect
+
+    if not isinstance(cssClss, list):
+      cssClss = [cssClss]
+    for cssCls in cssClss:
+      if inspect.isclass(cssCls):
+        self._report.cssObj.addPy(cssCls)
+        cssCls = cssCls.__name__
+      clssMod = self._report.style.add(cssCls)
+      if clssMod is not None:
+        self._report.style.cssCls(cssCls)
+    return self
+
+  def __str__(self):
+    styles = [s for s in self.pyStyle if not s.startswith('CssPivot')]
+    return '<div %(strAttr)s><div></div></div>%(helper)s' % {'strAttr': self.strAttr(pyClassNames=styles), "helper": self.helper}
+
+
+  # -----------------------------------------------------------------------------------------
+  #                                    MARKDOWN SECTION
+  # -----------------------------------------------------------------------------------------
+  @classmethod
+  def matchMarkDownBlock(cls, data):
+    return True if data[0].strip().startswith("---Pivot") else None
+
+  @staticmethod
+  def matchEndBlock(data):
+    return data.endswith("---")
+
+  @classmethod
+  def convertMarkDownBlock(cls, data, report=None):
+    return []
+
+  def jsMarkDown(self): return ""
+
