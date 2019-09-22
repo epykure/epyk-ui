5d182a7d419309eecdb8631d6a55730713afbce4
pyotr <smith.pyotr@gmail.com>
2019-09-22 10:51:56 +0100

    Update codepen_icons.py

--- epyk/tests/codepen/codepen_icons.py
+++ epyk/tests/codepen/codepen_icons.py
@@ -2,12 +2,14 @@ from epyk.core.Ares import Report
 from epyk.tests import test_statics
 
 rptObj = Report()
+
 rptObj.ui.icons.capture().color("red", "yellow")
 rptObj.ui.icons.table(tooltip="helper")
 rptObj.ui.icons.awesome("fas fa-stopwatch", tooltip="helper")
 
 rptObj.ui.icon("fab fa-css3-alt")
 rptObj.ui.images.badge("5", "Label", icon="fas fa-align-center")
+rptObj.ui.images.badge("New")
 
 
 rptObj.outs.browser.codepen(path=test_statics.OUTPUT_PATHS)
