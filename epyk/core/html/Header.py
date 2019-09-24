b0731032b9dd0265914093fbe90cab5a643febe1
marlyk <leemark0071@gmail.com>
2019-09-24 21:11:24 +0100

    Add header page

--- epyk/core/html/Header.py
+++ epyk/core/html/Header.py
@@ -9,9 +9,71 @@ class Meta(object):
 
   """
 
+  def charset(self):
+    """
+
+    :return:
+    """
+    return self
+
+
+
+  def author(self):
+    """
+
+    :return:
+    """
+    return self
+
+
+
+  def description(self):
+    """
+
+    :return:
+    """
+    return self
+
+  def keywords(self):
+    """
+
+    :return:
+    """
+    return self
+
+
+
+  def viewport(self):
+    """
+
+    :return:
+    """
+    return self
+
 
 class Header(object):
   """
 
   """
-  
+
+  @property
+  def title(self, title):
+    """
+
+    :return:
+    """
+    return
+
+  def base(self, url):
+    """
+    Specify a dedicated path for the relative paths in the page.
+    Basically the images will use this path as base if present in the page
+
+    Documentation
+    https://www.w3schools.com/tags/tag_base.asp
+
+    :param url:
+
+    :return:
+    """
+
