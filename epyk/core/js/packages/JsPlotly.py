e49dae0eb4d6f3dc26a7a7c76c7ca7d89af1b3bf
marlyk <leemark0071@gmail.com>
2019-10-17 22:32:22 +0100

    Plotly

--- epyk/core/js/packages/JsPlotly.py
+++ epyk/core/js/packages/JsPlotly.py
@@ -1,4 +1,74 @@
 """
 
 https://plot.ly/javascript/reference/
+https://plot.ly/javascript/plotlyjs-function-reference/#plotlynewplot
 """
+
+from epyk.core.js import JsUtils
+from epyk.core.js.primitives import JsObjects
+from epyk.core.js.packages import JsQuery
+from epyk.core.js.packages import JsPackage
+
+
+class JsPlotly(JsPackage):
+
+
+  def newPlot(self):
+    """
+
+    :return:
+    """
+
+  def react(self):
+    pass
+
+  def plot(self):
+    pass
+
+  def restyle(self):
+    pass
+
+  def relayout(self):
+    pass
+
+  def update(self):
+    pass
+
+  def validate(self):
+    pass
+
+  def makeTemplate(self):
+    pass
+
+  def validateTemplate(self):
+    pass
+
+  def addTraces(self):
+    pass
+
+  def deleteTraces(self):
+    pass
+
+  def moveTraces(self):
+    pass
+
+  def extendTraces(self):
+    pass
+
+  def prependTraces(self):
+    pass
+
+  def addFrames(self):
+    pass
+
+  def animate(self):
+    pass
+
+  def purge(self):
+    pass
+
+  def toImage(self):
+    pass
+
+  def downloadImage(self):
+    pass
