e4f5810872b0c7c96df99ee18dbab2358d927ece
marlyk <leemark0071@gmail.com>
2019-12-19 23:23:11 +0000

    Update CompForms.py

--- epyk/interfaces/components/CompForms.py
+++ epyk/interfaces/components/CompForms.py
@@ -9,9 +9,24 @@ class Forms(object):
   def __init__(self, context):
     self.context = context
 
-  def date(self, action, method, helper=None):
-    today = self.context.rptObj.ui.fields.today().set_attrs({"name": 'date'})
-    form = html.HtmlContainer.Form(self.context.rptObj, [today], action, method, helper)
+  def date(self, action, method, htmlCode="Current", helper=None):
+    col = self.context.rptObj.ui.col([self.context.rptObj.ui.fields.today(label=htmlCode).set_attrs({"name": htmlCode.upper()})])
+    col.css({"border": '1px solid %s' % self.context.rptObj.getColor("greys", 4),
+                                   "text-align": 'center', "width": 'none', "padding": '5px', "border-radius": '5px'})
+    form = html.HtmlContainer.Form(self.context.rptObj, [col], action, method, helper)
+    col += form.submit
+    self.context.register(form)
+    return form
+
+  def dates(self, action, method, htmlCode1="current", htmlCode2="Previous",  helper=None):
+    col = self.context.rptObj.ui.col([
+      self.context.rptObj.ui.fields.today(label=htmlCode1).set_attrs({"name": htmlCode1.upper()}),
+      self.context.rptObj.ui.fields.today(label=htmlCode2).set_attrs({"name": htmlCode2.upper()}),
+    ])
+    col.css({"border": '1px solid %s' % self.context.rptObj.getColor("greys", 4),
+                                   "text-align": 'center', "width": 'none', "padding": '5px', "border-radius": '5px'})
+    form = html.HtmlContainer.Form(self.context.rptObj, [col], action, method, helper)
+    col += form.submit
     self.context.register(form)
     return form
 
