0d64122fed6f95c1b1dbcc4539b45cafb46d8bae
marlyk <leemark0071@gmail.com>
2020-01-12 01:28:28 +0100

    Update JsEvents.py

--- epyk/core/js/objects/JsEvents.py
+++ epyk/core/js/objects/JsEvents.py
@@ -9,8 +9,59 @@ from epyk.core.js.primitives import JsNumber
 
 
 class Event(object):
+
+  def cancelBubble(self):
+    """
+    The cancelBubble() method prevents the event-flow from bubbling up to parent elements.
+
+    Documentation
+    https://www.w3schools.com/jsref/event_cancelbubble.asp
+
+    :return:
+    """
+    return JsString.JsString("event.cancelBubble = true")
+
+  def target(self):
+    """
+    Returns the element that triggered the event
+
+    Documentation
+    https://www.w3schools.com/jsref/event_target.asp
+
+    :return:
+    """
+    return JsString.JsString("event.target")
+
+  @property
+  def timeStamp(self):
+    """
+    Returns the time (in milliseconds relative to the epoch) at which the event was created
+
+    Documentation
+    https://www.w3schools.com/jsref/event_timestamp.asp
+
+    :return:
+    """
+    return JsString.JsString("event.timeStamp")
+
+  @property
+  def defaultPrevented(self):
+    """
+    The defaultPrevented event property checks whether the preventDefault() method was called for the event.
+
+    Documentation
+    https://www.w3schools.com/jsref/event_defaultprevented.asp
+
+    :return:
+    """
+    return JsString.JsString("event.defaultPrevented")
+
   def preventDefault(self):
     """
+    Cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur
+
+    Documentation
+    https://www.w3schools.com/jsref/event_preventdefault.asp
 
     :return:
     """
