c3f735b2fb258888893ff7b79f95de6f430f43ad
marlyk <leemark0071@gmail.com>
2020-01-18 19:31:03 +0000

    Update Properties.py

--- epyk/core/css/Properties.py
+++ epyk/core/css/Properties.py
@@ -18,12 +18,22 @@ class CssMixin(object):
 
   @display.setter
   def display(self, val):
-    """
-
-    :param val:
-    :return:
-    """
     if val is None:
       self.htmlObj.css({"display": 'None'})
     else:
       self.htmlObj.css({"display": val})
+
+  @property
+  def font_size(self):
+    """
+
+    :return:
+    """
+    return self.htmlObj.css("font-size")
+
+  @font_size.setter
+  def font_size(self, val):
+    if val is None:
+      self.htmlObj.css({"font-size": 'None'})
+    else:
+      self.htmlObj.css({"font-size": val})
