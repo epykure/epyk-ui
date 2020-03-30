bd217301a2a9ffeddf37e4e32c9b9b41aa18b5b6
marlyk <leemark0071@gmail.com>
2020-03-30 22:42:18 +0100

    JqueryUI

--- epyk/core/js/html/JsHtmlJqueryUI.py
+++ epyk/core/js/html/JsHtmlJqueryUI.py
@@ -2,7 +2,6 @@ from epyk.core.js.html import JsHtml
 from epyk.core.js.fncs import JsFncs
 from epyk.core.js import JsUtils
 
-from epyk.core.js.packages import JsQueryUi
 from epyk.core.js.primitives import JsObjects
 
 
@@ -17,28 +16,8 @@ class JsHtmlDatePicker(JsHtml.JsHtml):
   def content(self):
     return JsObjects.JsObjects.get('%s.val()' % self._src.dom.jquery.varId)
 
-  @property
-  def jqueryui(self):
-    """
-
-    :return:
-    :rtype: JsQueryUi.JQueryUiDatePicker
-    """
-    if self._jquery_ui is None:
-      self._jquery_ui = JsQueryUi.JQueryUiDatePicker(self._src)
-    return self._jquery_ui
-
 
 class JsHtmlProgressBar(JsHtml.JsHtml):
-  @property
-  def jqueryui(self):
-    """
-
-    :rtype: JsQueryUi.JsHtmlProgressBar
-    """
-    if self._jqueryui is None:
-      self._jqueryui = JsQueryUi.JQueryUiProgressBar(self._src)
-    return self._jqueryui
 
   @property
   def val(self):
