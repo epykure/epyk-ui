254d3a43169b8e3a7aa03949d2bd3a1a72666b27
Tinelsi972 <epykachu@gmail.com>
2020-04-13 20:19:58 +0100

    make some changes for documentation

---
+++ doc/generate_comp.py
@@ -0,0 +1,21 @@
+import os
+import importlib, inspect
+
+import sys
+sys.path.append(r'C:\Users\nelso\PycharmProjects\epyk-ui')
+
+for fol in ['components', 'geo', 'graphs', 'tables']:
+  for f in os.listdir(os.path.join(r'C:\Users\nelso\PycharmProjects\epyk-ui\epyk\interfaces', fol)):
+    if f.startswith('__'):
+      continue
+      
+    os.makedirs(fol, exist_ok=True)
+    mod_file = f.replace('.py', '')
+    mod = importlib.import_module("epyk.interfaces.%s.%s" % (fol, mod_file))
+    with open(r'%s\%s.rst' % (fol, mod_file), 'w') as doc_file:
+      for member, memclass in inspect.getmembers(mod, inspect.isclass):
+        title_str = '%s Interface' % member
+        doc_file.write('%s\n%s\n\n' % (title_str, '=' * len(title_str)))
+        doc_file.write('.. autoclass:: %s\n\t:members:' % str(memclass).split("'")[1])
+
+
