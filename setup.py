7aaabef4d918dc6a51e0b057f085e632ff4227cb
Tinelsi972 <epykachu@gmail.com>
2020-04-20 16:04:48 +0100

    commit documentation

--- setup.py
+++ setup.py
@@ -16,12 +16,15 @@ def install_required():
 setuptools.setup(
     name="epyk",
     author="epykure",
-    version="1.0.2",
+    version="1.0.3",
     author_email="smith.pyotr@gmail.com",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/epykure/epyk-ui",
+    project_urls = {
+    "Documentation": "www.epyk.io",
+    "Issue tracker": "https://github.com/epykure/epyk-ui/issues"},
     packages=setuptools.find_packages(),
     install_requires=install_required(),
     package_data={'epyk': [os.path.join('static', 'images', '*'), os.path.join('static', 'images', 'logo', '*')]},
