77af44687fd3beb38391f539d4f495044e16c8e7
Tinelsi972 <epykachu@gmail.com>
2020-05-22 20:23:18 +0100

    start working on Pyks

--- epyk/core/py/Pyk.py
+++ epyk/core/py/Pyk.py
@@ -3,6 +3,8 @@ import importlib, subprocess, sys, os, inspect
 class EpykMissingPykException(Exception):
   pass
 
+class EpykBoundRptObj(Exception):
+  pass
 
 class Pyk(object):
   """
@@ -11,10 +13,8 @@ class Pyk(object):
 
   __pyk_dict = None
   __instance = None
+  __rpt_obj = None
 
-
-  class __Pyk(object):
-    pass
 
   def __new__(cls):
     if cls.__instance is None:
@@ -22,11 +22,19 @@ class Pyk(object):
       cls.__pyk_dict = {}
     return cls.__instance
 
+  class __Pyk(object):
+
+    def register(self, rpt_obj):
+      for obj_name, obj in inspect.getmembers(self):
+        if obj_name.startswith('pyk_'):
+          rpt_obj.context.register(obj)
+
+
   @classmethod
   def requires(cls, pyk_file, components=None, autoinstall=False):
     """
     Description:
-    ------------------
+    ------------
 
       This function will allow you to import components from another pyk file.
       The pyk file can be located on your file system, or it can be on pypi or even github
@@ -48,7 +56,7 @@ class Pyk(object):
     if pyk_file.endswith('.py'):
       path_split = pyk_file.split(os.sep)
       sys.path.append(os.sep.join(path_split[:-1]))
-      mod = importlib.import_module(path_split[-1].replace('.py', ''))
+      importlib.import_module(path_split[-1].replace('.py', ''))
     else:
       try:
         importlib.import_module(pyk_file)
@@ -66,19 +74,33 @@ class Pyk(object):
     pyk_obj = cls.__Pyk()
     if components:
       for component in components:
-        setattr(pyk_obj, component, cls.__pyk_dict[pyk_file].get(component, 'Missing component'))
+        setattr(pyk_obj, 'pyk_%s' % component, cls.__pyk_dict[pyk_file].get(component, 'Missing component'))
     else:
       for obj_name, obj in cls.__pyk_dict[pyk_file].items():
-        print(obj_name, obj)
-        setattr(pyk_obj, obj_name, obj)
+        setattr(pyk_obj, 'pyk_%s' % obj_name, obj)
 
     return pyk_obj
 
-
-
   @classmethod
-  def exports(cls, obj_list=None):
-    pass
+  def exports(cls, obj__dict=None):
+    """
+    Description:
+    ------------
+
+      This function requries you to pass a dictionary with the object alias as key and the object as value
+      for as many objects as you wish
+      This will then be used by users who need to require your particular pyk file
+      You can pass epyk object in that dictionary as well as function if you need to
+    Usage:
+    ------
+
+        pyr.exports({'my_obj1': my_obj1, 'my_obj1': my_obj2})
+
+    :param obj__dict: dictionary with object name as key and object as value
+    """
+    cls()
+    for obj_name, obj in obj__dict.items():
+      cls.__pyk_dict[obj_name] = obj
 
 
 
