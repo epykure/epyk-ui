d68ca339da969c4c1dbd7fb32e2cbad24ac0e853
Tinelsi972 <epykachu@gmail.com>
2020-05-23 18:59:57 +0100

    add example scripts

--- epyk/tests/py/test_pyk.py
+++ epyk/tests/py/test_pyk.py
@@ -1,8 +1,16 @@
 from epyk.core.py import Pyk
 from epyk.core.Page import Report
+pyk_rpt = Pyk.requires(r'C:\Users\nelso\PycharmProjects\epyk-ui\epyk\tests\pyk_report.py')
 
 
 rpt_obj = Report()
-list_filter_object = Pyk.requires(r'C:\Users\nelso\Downloads\list_filter.py')
-Pyk.register(rpt_obj, [list_filter_object.list1, list_filter_object.search])
+
+rpt_obj.ui.text("Here we have a text followed by a normal button")
+
+Pyk.register(rpt_obj, pyk_rpt.button)
+
+rpt_obj.ui.text("Then we have an import button")
+
+Pyk.register(rpt_obj, pyk_rpt.imp)
+
 print(rpt_obj.outs.html_file())
