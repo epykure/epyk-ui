6675c94c30ee813e8b6f350f08c78049756e2018
marlyk <smith.pyotr@gmail.com>
2020-06-23 23:05:16 +0100

    Update react.py

--- epyk/web/react.py
+++ epyk/web/react.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 
 import subprocess
 
@@ -79,11 +80,11 @@ class NpxCli(object):
 
 class App(object):
 
-  def __init__(self, app_path, app_name, alias, name, report=None):
+  def __init__(self, app_path, app_name, alias, name, report=None, target_folder="views"):
     self.imports = {}
     self.vars, self.__map_var_names, self._report = {}, {}, report
     self._app_path, self._app_name = app_path, app_name
-    self.alias, self.__path, self.className, self.__components = alias, 'apps', name, None
+    self.alias, self.__path, self.className, self.__components = alias, target_folder, name, None
     self.comps = {}
 
   @property
@@ -104,6 +105,42 @@ class App(object):
     """
     return os.path.join("./", self.__path, self.name).replace("\\", "/")
 
+  def route(self, component, alias, path):
+    """
+    Description:
+    ------------
+    Add the app to the routing mechanism.
+    By default all the views are in a view folder within the Raact App.
+
+    Attributes:
+    ----------
+    :param component: String. The module name
+    :param alias: String. The url route
+    :param path: String. The .js module path
+    """
+    index_router = os.path.join(self._app_path, 'src', "index.js")
+    if not os.path.exists(index_router):
+      raise Exception("Problem with the React app")
+
+    with open(index_router) as f:
+      route = f.read()  # .split("\n\n")
+
+    if not component in route:
+      routes = route.split("import { Route, Link, BrowserRouter as Router } from 'react-router-dom';")
+      routes[0] = "%s\nimport %s from '%s';\n" % (routes[0].strip(), component, path.replace("./src", "."))
+      dis_route = routes[1].split("\n")
+      for i, line in enumerate(dis_route):
+        if line.strip().startswith("<Route "):
+          break
+
+      else:
+        raise Exception("Issue with file, please udpate the index.js manually")
+
+      route_end = dis_route[:i] + ['      <Route path="/%s" component={%s} />' % (alias, component)] + dis_route[i:]
+
+      with open(index_router, "w") as f:
+        f.write("\n".join([routes[0]] + ["import { Route, Link, BrowserRouter as Router } from 'react-router-dom';"] + route_end))
+
   def export(self, path=None, target_path=None):
     """
     Description:
@@ -118,7 +155,7 @@ class App(object):
     if target_path is None:
       target_path = []
     target_path.append(self.__path)
-    module_path = os.path.join(self._app_path, self._app_name, *target_path)
+    module_path = os.path.join(self._app_path, *target_path)
     if not os.path.exists(module_path):
       os.makedirs(module_path)
 
@@ -134,7 +171,7 @@ class App(object):
       f.write('''
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { %(builders)s } from './%(js_module)s.js'
+import { %(builders)s } from './%(js_module)s_modules.js'
 import './%(name)s.css';
 
 class %(alias)s extends React.Component {
@@ -147,7 +184,7 @@ export default AppRoot;
 ReactDOM.render(<%(alias)s />, document.getElementById('root'); %(jsFrgs)s);
 ''' % page)
 
-    with open(os.path.join(module_path, "%s.js" % page['js_module']), "w") as f:
+    with open(os.path.join(module_path, "%s_modules.js" % page['js_module']), "w") as f:
       for js_dep in JS_MODULES_IMPORTS:
         if js_dep in self._report.jsImports:
           f.write("%s\n" % JS_MODULES_IMPORTS[js_dep])
@@ -156,6 +193,43 @@ ReactDOM.render(<%(alias)s />, document.getElementById('root'); %(jsFrgs)s);
 
 
 class React(node.Node):
+
+  def create(self, name):
+    """
+    Description:
+    ------------
+    To create a new project, run:
+
+    Related Pages:
+
+      https://create-react-app.dev/docs/getting-started/
+
+    Attributes:
+    ----------
+    :param name: String. The application name
+    """
+    if name is None:
+      subprocess.run('npx create-react-app --help', shell=True, cwd=self._app_path)
+    else:
+      subprocess.run('npx create-react-app %s' % name, shell=True, cwd=self._app_path)
+
+  def serve(self, app_name, port=8081):
+    """
+    Description:
+    ------------
+    Return the version of React.js on the server
+
+    Related Pages:
+
+      https://create-react-app.dev/docs/getting-started/
+
+    Attributes:
+    ----------
+    :param app_name:
+    :param port:
+    """
+    path = os.path.join(self._app_path, app_name)
+    subprocess.run('npm start --port %s' % port, shell=True, cwd=path)
 
   def cli(self, app_name=None):
     """
@@ -174,7 +248,52 @@ class React(node.Node):
     """
     return NpxCli(self._app_path)
 
-  def page(self, selector=None, name=None, report=None, auto_route=False):
+  def router(self, app_name):
+    """
+    Description:
+    ------------
+    React Router is the de-facto React routing library, and it’s one of the most popular projects built on top of React.
+    This function will also update the module index.js in order to add the router automatically is missing
+
+    Related Pages:
+
+      https://flaviocopes.com/react-router/
+
+    Attributes:
+    ----------
+    :param app_name:
+    """
+    path = os.path.join(self._app_path, app_name, 'src', 'index.js')
+    #subprocess.run('npm install react-router-dom', shell=True, cwd=path)
+    with open(path) as f:
+      content = f.read()
+    if 'react-router-dom' not in content:
+      pass
+      new_files, with_router = [], False
+      for line in content.split("\n"):
+        if line.strip() and not line.startswith("import") and not with_router:
+          with_router = True
+          new_files.append("import { Route, Link, BrowserRouter as Router } from 'react-router-dom';")
+          new_files.append('''
+const routing = (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+    </div>
+  </Router>
+)
+''')
+        new_files.append(line)
+      new_files.append('''
+ReactDOM.render(
+  routing,
+  document.getElementById('root')
+);
+''')
+      with open(path, "w") as f:
+        f.write("\n".join(new_files))
+
+  def page(self, selector=None, name=None, report=None, auto_route=False, target_folder="apps"):
     """
     Description:
     ------------
@@ -194,10 +313,8 @@ class React(node.Node):
       if selector is None:
         selector = script.replace("_", "-")
     report = report or Page.Report()
-    report.framework("VUE")
-    self._page = App(self._app_path, self._app_name, selector, name, report=report)
-    #if auto_route:
-    #  self.route.add(self.__page.className, self.__page.alias, self.__page.path)
+    self._page = App(self._app_path, self._app_name, selector, name, report=report, target_folder=target_folder)
+    self.__route = auto_route
     return self._page
 
   def publish(self, app_name=None, target_path=None):
@@ -211,7 +328,20 @@ class React(node.Node):
     :param app_name:
     :param target_path: List  for example ['src', 'app']
     """
-    if self._page is not None:
-      self._page.export(target_path=target_path)
-    #if self.__route is not None:
-    #  self.route.export()
+    #if self._page is not None:
+    #  self._page.export(target_path=target_path)
+    if self.__route:
+      self._page.route(self._page.name, self._page.alias, self._page.path)
+
+  def home_page(self, report, app_name=None, with_router=False):
+    """
+    Description:
+    ------------
+    Change the Angular App home page
+
+    Attributes:
+    ----------
+    :param report:
+    :param app_name:
+    :param with_router:
+    """
