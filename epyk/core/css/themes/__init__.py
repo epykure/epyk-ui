be5f5b72c000dbf2c67d13ee872b1183ca8a01db
marlyk <smith.pyotr@gmail.com>
2021-03-27 18:19:20 +0000

    Update __init__.py

--- epyk/core/css/themes/__init__.py
+++ epyk/core/css/themes/__init__.py
@@ -1,3 +1,4 @@
+from . import Theme
 from . import ThemeDark as darks
 from . import ThemeRed as reds
 from . import ThemeGreen as greens
@@ -19,3 +20,163 @@ REGISTERED_THEMES = [
   {"value": 'ThemeRed.Red', 'name': '', 'content': DIV_STYLE % '#B71C1C'},
   {"value": 'ThemeRed.Pink', 'name': '', 'content': DIV_STYLE % '#d598a3'},
 ]
+
+
+class RegisteredThemes:
+
+  def __init__(self, page):
+    self.page = page
+
+  def default(self, index=5, step=1, ovr_attrs=None):
+    """
+    Description:
+    ------------
+    The default CSS Color theme.
+
+    Attributes:
+    ----------
+    :param index: Integer. Optional. The base color index.
+    :param step: Integer. Optional. The move step number.
+    :param ovr_attrs: Dictionary. Optional. The nested dictionary with color codes.
+    """
+    self.page.theme = Theme.ThemeDefault(ovr_attrs=ovr_attrs, index=index, step=step)
+
+  def blue(self, index=5, step=1, ovr_attrs=None):
+    """
+    Description:
+    ------------
+    The Blue CSS Color theme.
+
+    Attributes:
+    ----------
+    :param index: Integer. Optional. The base color index.
+    :param step: Integer. Optional. The move step number.
+    :param ovr_attrs: Dictionary. Optional. The nested dictionary with color codes.
+    """
+    self.page.theme = blues.Blue(ovr_attrs=ovr_attrs, index=index, step=step)
+
+  def blue_grey(self, index=5, step=1, ovr_attrs=None):
+    """
+    Description:
+    ------------
+    The Blue Grey CSS Color theme.
+
+    Attributes:
+    ----------
+    :param index: Integer. Optional. The base color index.
+    :param step: Integer. Optional. The move step number.
+    :param ovr_attrs: Dictionary. Optional. The nested dictionary with color codes.
+    """
+    self.page.theme = blues.BlueGrey(ovr_attrs=ovr_attrs, index=index, step=step)
+
+  def blue_light(self, index=5, step=1, ovr_attrs=None):
+    """
+    Description:
+    ------------
+    The Light Blue CSS Color theme.
+
+    Attributes:
+    ----------
+    :param index: Integer. Optional. The base color index.
+    :param step: Integer. Optional. The move step number.
+    :param ovr_attrs: Dictionary. Optional. The nested dictionary with color codes.
+    """
+    self.page.theme = blues.LightBlue(ovr_attrs=ovr_attrs, index=index, step=step)
+
+  def green(self, index=5, step=1, ovr_attrs=None):
+    """
+    Description:
+    ------------
+    The Green CSS Color theme.
+
+    Attributes:
+    ----------
+    :param index: Integer. Optional. The base color index.
+    :param step: Integer. Optional. The move step number.
+    :param ovr_attrs: Dictionary. Optional. The nested dictionary with color codes.
+    """
+    self.page.theme = greens.Green(ovr_attrs=ovr_attrs, index=index, step=step)
+
+  def green_light(self, index=5, step=1, ovr_attrs=None):
+    """
+    Description:
+    ------------
+    The light Green CSS Color theme.
+
+    Attributes:
+    ----------
+    :param index: Integer. Optional. The base color index.
+    :param step: Integer. Optional. The move step number.
+    :param ovr_attrs: Dictionary. Optional. The nested dictionary with color codes.
+    """
+    self.page.theme = greens.LightGreen(ovr_attrs=ovr_attrs, index=index, step=step)
+
+  def teal(self, index=5, step=1, ovr_attrs=None):
+    """
+    Description:
+    ------------
+    The Teal CSS Color theme.
+
+    Attributes:
+    ----------
+    :param index: Integer. Optional. The base color index.
+    :param step: Integer. Optional. The move step number.
+    :param ovr_attrs: Dictionary. Optional. The nested dictionary with color codes.
+    """
+    self.page.theme = greens.Teal(ovr_attrs=ovr_attrs, index=index, step=step)
+
+  def grey(self, index=5, step=1, ovr_attrs=None):
+    """
+    Description:
+    ------------
+    The Grey CSS Color theme.
+
+    Attributes:
+    ----------
+    :param index: Integer. Optional. The base color index.
+    :param step: Integer. Optional. The move step number.
+    :param ovr_attrs: Dictionary. Optional. The nested dictionary with color codes.
+    """
+    self.page.theme = darks.Grey(ovr_attrs=ovr_attrs, index=index, step=step)
+
+  def dark(self, index=5, step=1, ovr_attrs=None):
+    """
+    Description:
+    ------------
+    The Dark CSS Color theme.
+
+    Attributes:
+    ----------
+    :param index: Integer. Optional. The base color index.
+    :param step: Integer. Optional. The move step number.
+    :param ovr_attrs: Dictionary. Optional. The nested dictionary with color codes.
+    """
+    self.page.theme = darks.Dark(ovr_attrs=ovr_attrs, index=index, step=step)
+
+  def pink(self, index=5, step=1, ovr_attrs=None):
+    """
+    Description:
+    ------------
+    The Pink CSS Color theme.
+
+    Attributes:
+    ----------
+    :param index: Integer. Optional. The base color index.
+    :param step: Integer. Optional. The move step number.
+    :param ovr_attrs: Dictionary. Optional. The nested dictionary with color codes.
+    """
+    self.page.theme = reds.Pink(ovr_attrs=ovr_attrs, index=index, step=step)
+
+  def red(self, index=5, step=1, ovr_attrs=None):
+    """
+    Description:
+    ------------
+    The Red CSS Color theme.
+
+    Attributes:
+    ----------
+    :param index: Integer. Optional. The base color index.
+    :param step: Integer. Optional. The move step number.
+    :param ovr_attrs: Dictionary. Optional. The nested dictionary with color codes.
+    """
+    self.page.theme = reds.Red(ovr_attrs=ovr_attrs, index=index, step=step)
