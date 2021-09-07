45217ab3feb9989022bb2d4ee61b7ebb58a0e16a
marlyk <smith.pyotr@gmail.com>
2021-09-07 22:57:04 +0100

    Update __init__.py

--- epyk/core/py/__init__.py
+++ epyk/core/py/__init__.py
@@ -6,11 +6,41 @@ class OrderedSet(list):
 
   def discard(self, value):
     """
+    Description:
+    ------------
+    remove a specific class from the component class attributes.
 
-    :param value:
+    Attributes:
+    ----------
+    :param value: String. The class reference.
     """
     self.remove(value)
 
   def add(self, key):
+    """
+    Description:
+    ------------
+    Add a class to the component class attributes.
+
+    Class needs to be defined, this will just add the reference to the component.
+    Attributes:
+    ----------
+    :param key: String. The class name.
+    """
     if key not in self:
       self.append(key)
+
+  def initialise(self, classes):
+    """
+    Description:
+    ------------
+    Reset the classes for the component.
+    Remove all the default ones.
+
+    Attributes:
+    ----------
+    :param classes: List the classes to be added to the component class attribute
+    """
+    self.clear()
+    for cls in classes:
+      self.append(cls)
