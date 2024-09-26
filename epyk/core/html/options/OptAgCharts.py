64ad05f48a29945c6c781f134432425e518b0ca7
epykure <55317031+epykure@users.noreply.github.com>
2024-09-26 22:45:46 +0100

    Update OptAgCharts.py

--- epyk/core/html/options/OptAgCharts.py
+++ epyk/core/html/options/OptAgCharts.py
@@ -38,6 +38,42 @@ class AgChartsConfig(OptionsWithTemplates):
         self._config(values)
 
     @property
+    def height(self) -> int:
+        """The height of the chart in pixels.
+
+        `Related Pages <https://www.ag-grid.com/charts/options/#reference-AgChartOptions-height/>`_
+        """
+        return self._config_get()
+
+    @height.setter
+    def height(self, value: int):
+        self._config(value)
+
+    @property
+    def minHeight(self) -> int:
+        """Sets the minimum height of the chart. Ignored if height is specified.
+
+        `Related Pages <https://www.ag-grid.com/charts/options/#reference-AgChartOptions-minHeight/>`_
+        """
+        return self._config_get()
+
+    @minHeight.setter
+    def minHeight(self, value: int):
+        self._config(value)
+
+    @property
+    def minWidth(self) -> int:
+        """Sets the minimum width of the chart. Ignored if width is specified.
+
+        `Related Pages <https://www.ag-grid.com/charts/options/#reference-AgChartOptions-minWidth/>`_
+        """
+        return self._config_get()
+
+    @minWidth.setter
+    def minWidth(self, value: int):
+        self._config(value)
+
+    @property
     def series(self):
         """
         To have one (the first) grid reflect column changes in another (the second), place the first grid's options in
@@ -50,3 +86,28 @@ class AgChartsConfig(OptionsWithTemplates):
     @series.setter
     def series(self, values: list):
         self._config(values)
+
+    @property
+    def theme(self):
+        """A predefined theme name or an object containing theme overrides.
+
+        `Related Pages <https://www.ag-grid.com/charts/options/#reference-AgChartOptions-theme/>`_
+        `Theme API <https://www.ag-grid.com/charts/themes-api//>`_
+        """
+        return self._config_get()
+
+    @theme.setter
+    def theme(self, value):
+        self._config(value)
+
+    @property
+    def width(self) -> int:
+        """The width of the chart in pixels.
+
+        `Related Pages <https://www.ag-grid.com/charts/options/#reference-AgChartOptions-width/>`_
+        """
+        return self._config_get()
+
+    @width.setter
+    def width(self, value: int):
+        self._config(value)
